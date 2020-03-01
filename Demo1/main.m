15ab4bbbfb209953671565aea3e0eb306ae16458
Siarhei Burakouski <serge.burakovsky@gmail.com>
2020-03-01 21:34:59 +0300

    Initial Commit

---
+++ Demo1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Demo1
+//
+//  Created by Siarhei Burakouski on 3/1/20.
+//  Copyright © 2020 Siarhei Burakouski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
